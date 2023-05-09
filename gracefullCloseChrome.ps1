297dc357db8586bd5a318ff92996d44e8788491e
BenHaslett-WIOT <89446100+BenHaslett-WIOT@users.noreply.github.com>
2023-05-09 12:11:23 +1000

    Update gracefullCloseChrome.ps1
    Uploaded wrong script... :(

--- gracefullCloseChrome.ps1
+++ gracefullCloseChrome.ps1
@@ -1,17 +1,28 @@
-# Create an empty array to hold the results
-$results = @()
+# Get the main Chrome processes
+$chromeProcesses = Get-Process -Name chrome -ErrorAction SilentlyContinue
 
-# For each team, get the owners
-foreach ($team in $teams) {
-    $owners = Get-TeamUser -GroupId $team.GroupId -Role Owner
-    foreach ($owner in $owners) {
-        # Create a custom object and add it to the results
-        $results += New-Object PSObject -Property @{
-            'Team'  = $team.DisplayName
-            'Owner' = $owner.Name
+if ($chromeProcesses) {
+    # Define the SendMessage function from user32.dll
+    Add-Type @"
+        using System;
+        using System.Runtime.InteropServices;
+        public class User32 {
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+        }
+"@
+
+    # Define the WM_CLOSE message constant
+    $WM_CLOSE = 0x0010
+
+    # Iterate through the Chrome processes
+    foreach ($process in $chromeProcesses) {
+        # Get the main window handle (HWND) of the Chrome process
+        $hWnd = $process.MainWindowHandle
+
+        # If the window handle is not zero, send the WM_CLOSE message
+        if ($hWnd -ne [IntPtr]::Zero) {
+            [void][User32]::SendMessage($hWnd, $WM_CLOSE, [IntPtr]::Zero, [IntPtr]::Zero)
         }
     }
 }
-
-# Export the results to a CSV file
-$results | Export-Csv -Path 'TeamsOwners.csv' -NoTypeInformation
